31167c3e6bf76cd2f2b612b64bb92eefeb48383b
= <ehoward@mainstreamdata.com>
2019-05-29 21:39:20 -0600

    Refactor mock api

--- src/api/words/MockClient/index.ts
+++ src/api/words/MockClient/index.ts
@@ -5,42 +5,50 @@ import IWordsClient from "../IWordsClient";
 let currentWords;
 export default class MockWordsClient implements IWordsClient {
    getWords() {
-      return new Promise<string[]>((resolve, reject) => {
+      return new Promise<string[]>((resolve) => {
          currentWords = shuffle(WordList.split(/\n/g));
          resolve(currentWords);
       });
    }
 
-   checkWords(words: string, backspaceCount: number) {
-      return new Promise<WordsResult>((resolve, reject) => {
-         const user = words.trim().split(' ');
-         const answers = currentWords;
+   // If they didn't finish typing the last word, we don't want to mark it wrong
+   // unless the part they did type is wrong
+   isWordWrong(user, real, partial) {
+      const comp = partial ? real.substring(0, user.length) : real;
+      return comp !== user;
+   }
 
-         let keystrokes = words.length;
-         let wrong = 0;
-
-         for (var i = 0; i < user.length; i++) {
-            const userWord = user[i];
-            const realWord = answers[i];
-
-            if (i === user.length - 1) {
-               if (realWord.substring(0, userWord.length) !== userWord) {
-                  wrong++;
-               }
-            } else if (userWord !== realWord) {
-               wrong++;
-            }
+   getWrongWordCount(user, answers) {
+      let wrong = 0;
+      for (var i = 0; i < user.length; i++) {
+         if(!answers[i]){
+            // There were more words typed than exist
+            console.error("Ran out of words");
+            break;
          }
+         
+         if (this.isWordWrong(user[i], answers[i], i === user.length - 1)) {
+            wrong++;
+         }
+      }
 
-         keystrokes += backspaceCount;
+      return wrong;
+   }
 
-         const wpm = Math.round(keystrokes / 5);
+   getWPM(keystrokes) {
+      return Math.round(keystrokes / 5);
+   }
+
+   checkWords(words: string, backspaceCount: number) {
+      return new Promise<WordsResult>((resolve) => {
+         const wpm = this.getWPM(words.length + backspaceCount);
+         const wrong = this.getWrongWordCount(words.trim().split(' '), currentWords);
 
          if (wpm > 130) {
             // good time to check for bots
             console.log('too fast');
          }
-         
+
          resolve({ wpm, wrong });
       });
    }
